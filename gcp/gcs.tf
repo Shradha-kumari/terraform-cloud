629b6862dab1c54641134cdfc2a3b87b523a4336
Shradha Kumari <shradhakumari@Shradhas-MacBook-Air.local>
2025-06-24 20:59:02 +0530

    ok

--- gcp/gcs.tf
+++ gcp/gcs.tf
@@ -1,7 +1,7 @@
 module "cloud-storage" {
   source  = "terraform-google-modules/cloud-storage/google"
   version = "10.0.1"
-  count = legth(var.buckets)>0 ? 1:0
+  count = length(var.buckets)>0 ? 1:0
   names = var.buckets
   project_id = var.project_id
   
